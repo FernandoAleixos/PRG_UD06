1a4e1e7c0a047864f9d5ddc0e64a47361ed07428
FernandoAleixos <fernanaleixos0204@gmail.com>
2022-03-24 00:41:56 +0100

    Atributos clase Vuelo.java

--- src/gestorVuelos/Vuelo.java
+++ src/gestorVuelos/Vuelo.java
@@ -9,5 +9,15 @@ public class Vuelo {
     private String destino;
     private LocalTime hSalida;
     private LocalTime hLlegada;
-    private 
+    private String[] asiento;
+    private int numP;
+
+    public Vuelo(String id, String orig, String dest, LocalTime hsal, LocalTime hlleg) {
+        this.identificador = id;
+        this.origen = orig;
+        this.destino = dest;
+        this.hSalida = hsal;
+        this.hLlegada = hlleg;
+
+    }
 }
