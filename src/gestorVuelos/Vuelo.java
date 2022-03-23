7e82376929b708a08f2e81277c663239092a671c
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-23 13:59:11 +0100

    Initial commit

---
+++ src/gestorVuelos/Vuelo.java
@@ -0,0 +1,13 @@
+package gestorVuelos;
+
+import java.time.LocalTime;
+
+public class Vuelo {
+
+    private String identificador;
+    private String origen;
+    private String destino;
+    private LocalTime hSalida;
+    private LocalTime hLlegada;
+    private 
+}
