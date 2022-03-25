75916007704c050e7e2df9684c058b6808d50c7f
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-25 12:53:03 +0100

    Vuelo.java - 25/03/2022

--- src/gestorVuelos/Vuelo.java
+++ src/gestorVuelos/Vuelo.java
@@ -4,6 +4,8 @@ import java.time.LocalTime;
 
 public class Vuelo {
 
+    private static final int MAX_PASAJEROS = 50;
+
     private String identificador;
     private String origen;
     private String destino;
@@ -12,12 +14,54 @@ public class Vuelo {
     private String[] asiento;
     private int numP;
 
+    //Constructor que crea un vuelo con identificador, ciudad de
+    //origen, ciudad de destino, hora de salida y hora de llegada indicados en los
+    //respectivos parámetros, y sin pasajeros.
     public Vuelo(String id, String orig, String dest, LocalTime hsal, LocalTime hlleg) {
         this.identificador = id;
         this.origen = orig;
         this.destino = dest;
         this.hSalida = hsal;
         this.hLlegada = hlleg;
-        this.asiento = new String[10];
+        this.asiento = new String[MAX_PASAJEROS + 1];
+        this.numP = 0;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public boolean hayLibres() {
+        return (this.numP < MAX_PASAJEROS);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vuelo vuelo = (Vuelo) o;
+
+        return identificador != null ? identificador.equals(vuelo.identificador) : vuelo.identificador == null;
+    }
+
+    public int reservarAsiento(String pas, char pref) throws VueloCompletoException{
+        if (hayLibres()) {
+
+        } else {
+            throw new VueloCompletoException();
+        }
+        return ;
+    }
+
+    private class VueloCompletoException extends Exception {
     }
 }
