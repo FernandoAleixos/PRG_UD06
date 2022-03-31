34503b69241ad47fe3aecad1ee0890afc27824a5
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-31 08:52:26 +0200

    Actividades ficheros, corrección - 30/03/2022

---
+++ src/Actividades/EscribirFichero2.java
@@ -0,0 +1,30 @@
+package Actividades;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class EscribirFichero2 {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        try (FileWriter escribir = new FileWriter(new File("src/texto.txt"));) {
+            System.out.println("Introduce tu nombre: ");
+            String nombre = tec.nextLine();
+            escribir.write(nombre);
+        } catch (IOException e) {
+            System.out.println("Problema al escribir en el archivo.");
+        }
+
+        try (FileReader leer = new FileReader("src/texto.txt")) {
+            int caracter;
+            while ((caracter = leer.read()) != -1) {
+                char letra = (char) caracter;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
