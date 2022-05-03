a0d9be712584cb2f49b4085e3b1b9ae31106fa6a
AleixosFernando <fernandoaleixos@iesmre.com>
2022-05-03 09:44:43 +0200

    Colecciones - 03/04/2022

--- src/colecciones/Actividad1.java
+++ src/colecciones/Actividad1.java
@@ -23,28 +23,28 @@ public class Actividad1 {
 
 
         //Mostrar el valor de la posición 5
-        System.out.println("\n-Mostrar el valor de la posición 5\n" +
+        System.out.print("\n-Mostrar el valor de la posición 5\n" +
                 misNumeros.get(misNumeros.indexOf(5)));
 
         //Añadir el valor 8 en la posición 4
-        System.out.println("\n-Añadir el valor 8 en la posición 4\n");
+        System.out.print("\n-Añadir el valor 8 en la posición 4\n");
         misNumeros.add(4, 8);
         System.out.println(misNumeros.toString());
 
         //Cambiar el valor de la posición 1 por 9
-        System.out.println("\n-Cambiar el valor de la posición 1 por 9\n");
+        System.out.print("\n-Cambiar el valor de la posición 1 por 9\n");
         misNumeros.set(1, 9);
 
         //Eliminar el valor 5. (misNumeros.remove(new Integer(5))
-        System.out.println("\n-Eliminar el valor 5. (misNumeros.remove(new Integer(5))\n");
+        System.out.print("\n-Eliminar el valor 5. (misNumeros.remove(new Integer(5))\n");
         misNumeros.remove(misNumeros.indexOf(5));
 
         //Eliminar el valor de la posición 3
-        System.out.println("\n-Eliminar el valor de la posición 3\n");
+        System.out.print("\n-Eliminar el valor de la posición 3\n");
         misNumeros.remove(3);
 
         //Recorrer el array con un bucle for
-        System.out.println("\n-Recorrer el array con un bucle for\n");
+        System.out.print("\n-Recorrer el array con un bucle for\n");
         for (Integer j : misNumeros) {
             int numPos = 0;
             numPos++;
@@ -52,15 +52,59 @@ public class Actividad1 {
         }
 
         //Recorrer el array con un bucle Iterator
-        System.out.println("\n-Recorrer el array con un bucle Iterator\n");
+        System.out.print("\n-Recorrer el array con un bucle Iterator\n");
         Iterator<Integer> it = misNumeros.iterator();
         for (Iterator<Integer> iter = it; iter.hasNext(); ) {
+            iter.next();
             System.out.println("iterator");
         }
 
         //Comprobar si existe el elemento 0
-        System.out.println("\n-Comprobar si existe el elemento 0\n" +
-                misNumeros.contains(0));
+        System.out.print("\n-Comprobar si existe el elemento 0\n");
+        if (misNumeros.contains(0)) {
+            System.out.println(0);
+        }
 
+        //Comprobar si existe el elemento 7
+        System.out.print("\n-Comprobar si existe el elemento 7\n");
+        if (misNumeros.contains(7)) {
+            System.out.println(7);
+        }
+
+        //Clonar el ArrayListmisNumeros en otro llamado copiaArrayList
+        ArrayList <Integer> copiaArrayList = new ArrayList<>();
+        copiaArrayList = (ArrayList<Integer>) misNumeros.clone();
+
+        //Añadir el elemento 9
+        misNumeros.add(9);
+
+        //Mostrar la posición de la primera ocurrencia del elemento 9
+        misNumeros.indexOf(9);
+
+        //Mostrar la posición de la última ocurrencia del elemento 9
+        System.out.print("\n-Mostrar la posición de la última ocurrencia del elemento 9\n" +
+                misNumeros.lastIndexOf(9));
+
+        //Borrar todos los elementos del ArrayListcopiaArrayList
+        System.out.print("\n-Borrar todos los elementos del ArrayListcopiaArrayList\n");
+        copiaArrayList.clear();
+
+        //Comprobar si el ArrayListcopiaArrayList está vacio
+        System.out.print("\n-Comprobar si el ArrayListcopiaArrayList está vacio\n");
+        if (copiaArrayList.isEmpty()) {
+            System.out.println("Vacío");
+        } else {
+            System.out.println("El array contiene elementos.");
+        }
+
+        //Convertir el ArrayListmisNumeros en un Array y recorrerlo con un bucle mejorado
+        Integer[] array = new Integer[misNumeros.size()];
+        int i = 0;
+
+        for (Integer f : array) {
+            array[i] = misNumeros.get(i);
+            System.out.print(array[i] + ", ");
+            i++;
+        }
     }
 }
---
+++ src/colecciones/Actividad2.java
@@ -0,0 +1,22 @@
+/*
+  Un cine precisa una aplicación para controlar las personas de la cola para losestrenos de  películas.
+  Debemos crear  una  lista  con  la  edad  de  las  personas de  la  lista  y
+  tendremos que calcular la entrada según la edad de la persona (mínimo 5 años).
+  Para la edad dela persona se generan aleatoriamente números entre 5 y 60 años.
+  Al final, deberemos mostrar lacantidad total recaudada.
+  El número de personas de la cola se elige al azar entre 0 y 50.
+*/
+package colecciones;
+
+import java.util.LinkedList;
+
+public class Actividad2 {
+
+    final int ALEATORIS = (int) (Math.random() * 55) + 5;
+
+    public static void main(String[] args) {
+        LinkedList <Integer> colaCine = new LinkedList<>();
+
+
+    }
+}
