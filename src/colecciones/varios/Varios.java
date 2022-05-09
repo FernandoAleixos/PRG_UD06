56338fe5964499562e0acec92c7d4eb636866a35
FernandoAleixos <fernanaleixos0204@gmail.com>
2022-05-10 00:29:02 +0200

    Package Varios 09/05/2022

--- src/colecciones/varios/Varios.java
+++ src/colecciones/varios/Varios.java
@@ -1,8 +1,26 @@
 package colecciones.varios;
 
+import java.util.Arrays;
+
 public class Varios {
 
-    public static int quitarDuplicados(int v[]) {
+    public static int[] quitarDuplicados(int v[]) {
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length; j++) {
+                if (v[i] == v[j]) {
+                    v[j] = 0;
+                }
+            }
+        }
+        Arrays.sort(v);
+        return v;
+    }
+
+    public static int[] union1(int[] v1, int[] v2) {
+        int[] arrayFinal = new int[v1.length + v2.length];
+
+        quitarDuplicados(v1);
+        quitarDuplicados(v2);
 
 
         return ;
