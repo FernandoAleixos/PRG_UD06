4a4f750a8c3a23c83f38519950e85c9ea4700514
AleixosFernando <fernandoaleixos@iesmre.com>
2022-05-11 10:19:04 +0200

    Colecciones - 11/05/2022

--- src/colecciones/varios/Varios.java
+++ src/colecciones/varios/Varios.java
@@ -1,28 +1,39 @@
 package colecciones.varios;
 
+import java.security.CodeSource;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class Varios {
 
     public static int[] quitarDuplicados(int v[]) {
+        LinkedHashSet<Integer> listado = new LinkedHashSet<>();
+
         for (int i = 0; i < v.length; i++) {
-            for (int j = 0; j < v.length; j++) {
-                if (v[i] == v[j]) {
-                    v[j] = 0;
-                }
-            }
+            listado.add(v[i]);
         }
-        Arrays.sort(v);
-        return v;
+
+        int[] sinDuplicados = new int[listado.size()];
+
+        int i = 0;
+        for (Integer f : listado) {
+            sinDuplicados[i++] = f;
+        }
+
+        return sinDuplicados;
     }
 
     public static int[] union1(int[] v1, int[] v2) {
-        int[] arrayFinal = new int[v1.length + v2.length];
 
-        quitarDuplicados(v1);
-        quitarDuplicados(v2);
+    }
 
+    public static int[] union2() {
 
-        return ;
+    }
+
+    public static void main(String[] args) {
+        int[] v = {1, 2, 3 ,4, 4};
+        quitarDuplicados(v);
     }
 }
